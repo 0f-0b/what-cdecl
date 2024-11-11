c7c02927e8f84a75488da13bd4cdc85956b56834
ud2 <sjx233@qq.com>
2024-11-11 10:09:15 +0800

    remove coercing

--- static/collections/sample.ts
+++ static/collections/sample.ts
@@ -1,6 +1,4 @@
-import { toLength } from "./to_length.ts";
-
 export function sample<T>(arr: ArrayLike<T>): T | undefined {
-  const length = toLength(arr.length);
+  const length = arr.length;
   return length === 0 ? undefined : arr[Math.floor(Math.random() * length)];
 }
